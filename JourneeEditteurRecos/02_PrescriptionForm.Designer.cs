5e4b9f1c5b432c538cd50f5c7276198c9544f8b1
Nicolas CAUVET <nicolascauvet@gmail.com>
2014-04-03 13:56:10 +0200

    Add rpescriptionForm

---
+++ JourneeEditteurRecos/02_PrescriptionForm.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace JourneeEditteurRecos
+{
+    partial class PrescriptionForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+     
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.patientControl1 = new ExemplePoso.PatientControl();
+            this.RecosListPanel = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.RecosListPanel.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.patientControl1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(449, 858);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PatientPanel";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(368, 579);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // patientControl1
+            // 
+            this.patientControl1.Location = new System.Drawing.Point(5, 32);
+            this.patientControl1.Name = "patientControl1";
+            this.patientControl1.Size = new System.Drawing.Size(444, 541);
+            this.patientControl1.TabIndex = 0;
+            // 
+            // RecosListPanel
+            // 
+            this.RecosListPanel.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.RecosListPanel.Controls.Add(this.listBox2);
+            this.RecosListPanel.Controls.Add(this.listBox1);
+            this.RecosListPanel.Location = new System.Drawing.Point(451, 12);
+            this.RecosListPanel.Name = "RecosListPanel";
+            this.RecosListPanel.Size = new System.Drawing.Size(243, 858);
+            this.RecosListPanel.TabIndex = 16;
+            this.RecosListPanel.TabStop = false;
+            this.RecosListPanel.Text = "RecosListPanel";
+            // 
+            // listBox2
+            // 
+            this.listBox2.DisplayMember = "name";
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(17, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(220, 290);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.ValueMember = "name";
+            this.listBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseDoubleClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.DisplayMember = "name";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(17, 333);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 511);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.ValueMember = "name";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightCoral;
+            this.groupBox2.Controls.Add(this.webBrowser1);
+            this.groupBox2.Location = new System.Drawing.Point(700, 345);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(877, 525);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "RecosPanel";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 16);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(871, 506);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Turquoise;
+            this.groupBox3.Controls.Add(this.listBox5);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Location = new System.Drawing.Point(700, 222);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(877, 117);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "PrescriptionPanel";
+            // 
+            // listBox5
+            // 
+            this.listBox5.DisplayMember = "name";
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(106, 16);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(753, 82);
+            this.listBox5.TabIndex = 3;
+            this.listBox5.ValueMember = "name";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(22, 77);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Reset";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(22, 48);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "<<";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(22, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = ">>";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBox4.Controls.Add(this.listBox4);
+            this.groupBox4.Controls.Add(this.listBox3);
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.button2);
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Location = new System.Drawing.Point(700, 16);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(877, 200);
+            this.groupBox4.TabIndex = 19;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "SearchPanel";
+            // 
+            // listBox4
+            // 
+            this.listBox4.DisplayMember = "name";
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(234, 84);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(204, 95);
+            this.listBox4.TabIndex = 5;
+            this.listBox4.ValueMember = "name";
+            this.listBox4.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox4_MouseDoubleClick);
+            // 
+            // listBox3
+            // 
+            this.listBox3.DisplayMember = "name";
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(22, 84);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(204, 95);
+            this.listBox3.TabIndex = 4;
+            this.listBox3.ValueMember = "name";
+            this.listBox3.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox3_MouseDoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(234, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Drugs";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Indications";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SearchRcosByName";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(416, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // PrescriptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1589, 882);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.RecosListPanel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "PrescriptionForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.RecosListPanel.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox RecosListPanel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private ExemplePoso.PatientControl patientControl1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox5;
+    }
+}
+
---
+++ JourneeEditteurRecos/02_PrescriptionForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using VidalAPI.Services;
+using VidalAPI.Domain;
+
+namespace JourneeEditteurRecos
+{
+    public partial class PrescriptionForm : Form
+    {
+        static VidalAPI.ProductLine vidalProduct = VidalAPI.ProductLine.GetProduct(VidalAPI.ProductLineID.VIDALEXPERT_PRODUCTID);
+        public PrescriptionForm()
+        {
+            InitializeComponent();
+            InitDomain();
+            patientControl1.setVidalProduct(vidalProduct);
+
+        }
+
+        private void InitDomain()
+        {
+            listBox2.DataSource = vidalProduct.GetService<RecoService>().SearchAllDomains().ToArray();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource = vidalProduct.GetService<RecoService>().SearchByName(textBox1.Text).ToArray();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Reco reco = ((Reco)listBox1.SelectedItem);
+            if (reco != null && reco.UrlAsString != null && reco.UrlAsString!="")
+            {
+                webBrowser1.Navigate(reco.UrlAsString);
+            }
+            else
+            {
+                webBrowser1.Navigate(vidalProduct.GetService<RecoService>().GetRecoUrl(reco.Id));
+            }
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            listBox1.DataSource = vidalProduct.GetService<RecoService>().SearchByDomainIds(new List<int>(){((RecoDomain)listBox2.SelectedItem).Id}).ToArray();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox3.DataSource = vidalProduct.GetService<IndicationGroupService>().SearchByName(textBox1.Text).ToArray();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox4.DataSource = vidalProduct.GetService<ProductService>().SearchByName(textBox1.Text).ToArray();
+        }
+
+        private void listBox3_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            listBox1.DataSource = vidalProduct.GetService<RecoService>().SearchByIndicationGroupIds(new List<int>(){((IndicationGroup)listBox3.SelectedItem).Id}).ToArray();
+        }
+
+        private void listBox4_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            listBox1.DataSource = vidalProduct.GetService<RecoService>().SearchAllRecosByProductIds(new List<int>(){((Product)listBox4.SelectedItem).Id }).ToArray();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource =  vidalProduct.GetService<DrugPrescriptionAnalysisService>().SearchRecosByPrescriptions(patientControl1.getPatientJson(), new List<string>()).Select(l=>l.Recommendation).ToArray();
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Product product = ((Product)listBox4.SelectedItem);
+            listBox5.Items.Add(product);
+            computeContextualRecos();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (listBox5.SelectedItem!=null){
+            listBox5.Items.Remove(listBox5.SelectedItem);
+            }
+            computeContextualRecos();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+         listBox5.Items.Clear();
+         computeContextualRecos();
+        }
+        private void computeContextualRecos()
+        {
+            List<string> prescription = new List<string>();
+            foreach (Product product in listBox5.Items)
+            {
+                prescription.Add(vidalProduct.GetService<DrugPrescriptionAnalysisService>().PrescriptionLineFromObjToJson(-1,-1,PosologyFrequencyType.POSOLOGYFREQUENCYTYPE_JNULL,new List<int>(),new List<int>(),product.Id,DrugType.DRUGTYPE_PRODUCT));
+            }
+            listBox1.DataSource = vidalProduct.GetService<DrugPrescriptionAnalysisService>().SearchRecosByPrescriptions(patientControl1.getPatientJson(), new List<string>()).Select(l=>l.Recommendation).ToArray();
+             
+        }
+    }
+        
+}
